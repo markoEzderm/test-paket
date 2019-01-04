3b7b10925697866c9ff85ced735265b131e50e53
Marko Micic <marko.micic@ezderm.com>
2019-01-04 15:02:00 +0100

    Added dist folder

---
+++ dist/test-kom.umd.js
@@ -0,0 +1,19 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('react')) :
+  typeof define === 'function' && define.amd ? define(['react'], factory) :
+  (global = global || self, factory(global.React));
+}(this, function (React) { 'use strict';
+
+  React = React && React.hasOwnProperty('default') ? React['default'] : React;
+
+  class TestKom extends React.Component {
+    render() {
+      return React.createElement(
+        'div',
+        null,
+        'Danas je lep dan da se napravi paket',
+      );
+    }
+  }
+
+}));
