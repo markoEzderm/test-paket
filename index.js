1faa4c7627f06954c4ae78863c9aab4d0167e1ca
Marko Micic <marko.micic@ezderm.com>
2019-01-04 14:44:14 +0100

    Initial component

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TestKom extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Danas je lep dan da se napravi paket',
+    );
+  }
+}
